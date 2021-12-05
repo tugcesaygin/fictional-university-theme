6322c3d9a7b4439e5d906caf29de49576c51e408
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2021-12-05 17:28:01 +0300

    changes

--- fictional-university-theme/header.php
+++ fictional-university-theme/header.php
@@ -54,18 +54,5 @@
         </div>
       </div>
     </header>
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(images/library-hero.jpg)"></div>
-      <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--large">Welcome!</h1>
-        <h2 class="headline headline--medium">We think you’ll like it here.</h2>
-        <h3 class="headline headline--small">Why don’t you check out the <strong>major</strong> you’re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
-      </div>
-    </div>
-    <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--large">Welcome!</h1>
-        <h2 class="headline headline--medium">We think you’ll like it here.</h2>
-        <h3 class="headline headline--small">Why don’t you check out the <strong>major</strong> you’re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
-      </div>
+    
+    
